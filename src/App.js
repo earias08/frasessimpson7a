c0a8ccbd56e6571e2a65b4585bfa1066e10998c4
Emilse <emilsearias@gmail.com>
2021-04-09 00:04:35 -0300

    se agrego la logica al boton para consultar la api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import Frase from './components/Frase';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
 
 function App() {
+  // crear el state
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    //  aqui traigo los datos de la api
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async() => {
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const resultado = await respuesta.json();
+    console.log(resultado[0]);
+    setPersonaje(resultado[0]);
+  };
+
   return (
-   <section className='container d-flex flex-column my-5 align-items-center'>
-     <img src={process.env.PUBLIC_URL+'logo.png'} alt="logo de los simpsons" className='w-75'/>
-     <Button variant="warning" className='my-4 w-75'>Obtener frase</Button>
-     <Frase></Frase>
-   </section>
+    <section className="container d-flex flex-column my-5 align-items-center">
+      <img
+        src={process.env.PUBLIC_URL + "logo.png"}
+        alt="logo de los simpsons"
+        className="w-75"
+      />
+      <Button variant="warning" className="my-4 w-75" onClick={() => consultarAPI()}>
+        Obtener frase
+      </Button>
+      <Frase personaje={personaje}></Frase>
+    </section>
   );
 }
 
