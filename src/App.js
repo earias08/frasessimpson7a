7fa8c7a79622cc835a685ce80acc547b42f3af8b
Emilse <emilsearias@gmail.com>
2021-04-13 17:26:06 -0300

    se agrego el componente spinner con su logica

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
 import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
   // crear el state
   const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
     //  aqui traigo los datos de la api
@@ -14,11 +16,20 @@ function App() {
   }, []);
 
   const consultarAPI = async() => {
+    // mostrar spinner
+    setCargando(true);
     const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
     const resultado = await respuesta.json();
     console.log(resultado[0]);
-    setPersonaje(resultado[0]);
+    // mostrar el componente frase
+    setTimeout(()=>{
+      setCargando(false);
+      setPersonaje(resultado[0]);
+    },2500);
   };
+
+  // (condicion logica)?(codigo cuando es verdadera):(codigo cuando la condicion es falsa) 
+  const mostrarComponente = (cargando === true)?(<Spinner></Spinner>):(<Frase personaje={personaje}></Frase>)
 
   return (
     <section className="container d-flex flex-column my-5 align-items-center">
@@ -30,7 +41,7 @@ function App() {
       <Button variant="warning" className="my-4 w-75" onClick={() => consultarAPI()}>
         Obtener frase
       </Button>
-      <Frase personaje={personaje}></Frase>
+    {mostrarComponente}
     </section>
   );
 }
